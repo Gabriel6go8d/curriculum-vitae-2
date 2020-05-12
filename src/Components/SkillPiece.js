435b1e5f3713ecedc8210955573351d66078994e
Gabriel Gutierrez <35079758+Gabriel6go8d@users.noreply.github.com>
2020-05-12 18:03:36 +0200

    CV Update

--- src/Components/SkillPiece.js
+++ src/Components/SkillPiece.js
@@ -1,15 +1,38 @@
 import React from 'react'
+import {FaLaptopCode, FaNewspaper, FaCogs, FaMobileAlt} from 'react-icons/fa'
 
 function SkillPiece(prop) {
 
+    const colorIcons = '#2881b8'
+    const sizeIcons = '60px'   
+    const colorBubble = '#a2d6f7' //#D9D9D9 light-gray and #a2d6f7 light-blue
+
     const toShow = prop.data.map((xx, index) => <div className='mr-2 mt-1' key={index}
-                                        style={{backgroundColor: '#a2d6f7', borderRadius: '8px'}}>
+                                        style={{backgroundColor: colorBubble, borderRadius: '8px'}}>
                                         <p className='ml-2 mr-2 mt-0' style={{fontSize:'15px', marginBottom: '0px'}}>{xx}</p></div>)
 
+    var icon = '' 
+    switch(prop.icon){
+        case 'laptop':
+            icon = <FaLaptopCode size={sizeIcons} color={colorIcons}/>
+            break
+        case 'paper':
+            icon = <FaNewspaper size={sizeIcons} color={colorIcons}/>
+            break
+        case 'cogs':
+            icon = <FaCogs size={sizeIcons} color={colorIcons}/>
+            break
+        case 'mobil':
+            icon = <FaMobileAlt size={sizeIcons} color={colorIcons}/>
+            break
+        default:
+            break
+    }
+
     return (
-        <div className='row justify-content-center m-0 p-0 mt-3 mb-5'>
+        <div className='row justify-content-center m-0 p-0 mt-3 mb-5' style={{minHeight: '130px'}}>
             <div className='col-4'>
-                {prop.icon}
+                {icon}
             </div>
             <div className='col-6 m-0 p-0 text-left'>
                 <h5>{prop.label}</h5>
--- src/Components/ViewSkills.js
+++ src/Components/ViewSkills.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {FaLaptopCode, FaNewspaper, FaCogs, FaMobileAlt} from 'react-icons/fa'
 import SkillPiece from './SkillPiece'
 import Text from './Text.json'
 
@@ -12,23 +11,23 @@ function ViewSkills() {
         TextSelect = Text[0].skill
     }else{
         TextSelect = Text[1].skill
-    }
+    }    
 
     return (
         <div className='view_expose text-left'>
             <div className='row m-0 p-0 justify-content-center skill_section'>
                 <h5 className='col-11 section_title mt-4 p-0'>{TextSelect.title}</h5>
 
-                <div className='col-12 col-md-5 m-0 p-0'>
-                    <SkillPiece label={TextSelect.sk11} data={['HTML', 'CSS', 'Javascript']} icon={<FaLaptopCode size='60px' color='#2881b8'/>}/>
-                    <SkillPiece label={TextSelect.sk12} data={['Bootstrap', 'React']} icon={<FaNewspaper size='60px' color='#2881b8'/>}/> 
+                <div className='col-12 col-md-6 m-0 p-0'>
+                    <SkillPiece label={TextSelect.sk11} data={['HTML', 'CSS', 'Javascript']} icon='laptop'/>
+                    <SkillPiece label={TextSelect.sk12} data={['Bootstrap', 'React']} icon='paper'/> 
                 </div>
                 
-                <div className='vertical_divider2 movilhidde'></div>  
+                <div className='vertical_divider_skills movilhidde'></div>  
 
-                <div className='col-12 col-md-5 m-0 p-0'>
-                    <SkillPiece label={TextSelect.sk13} data={['Laravel', 'PHP', 'MySQL', 'Axios']} icon={<FaCogs size='60px' color='#2881b8'/>}/>
-                    <SkillPiece label={TextSelect.sk14} data={['Swift']} icon={<FaMobileAlt size='60px' color='#2881b8'/>}/> 
+                <div className='col-12 col-md-6 m-0 p-0'>
+                    <SkillPiece label={TextSelect.sk13} data={['Laravel', 'PHP', 'MySQL', 'Axios']} icon='cogs'/>
+                    <SkillPiece label={TextSelect.sk14} data={['Swift']} icon='mobil'/> 
                 </div>              
             </div>
         </div>
